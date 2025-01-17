51ee5519af1647d0e36493306305dc0238208fe8
unknown <Ashik-Rahman100>
2025-01-18 00:02:07 +0600

    customer stories added

--- src/components/Home/Stories/Stories.tsx
+++ src/components/Home/Stories/Stories.tsx
@@ -1,7 +1,15 @@
+import StoriesCard from "./StoriesCard";
+
 export default function Stories() {
   return (
-    <div className="px-3 lg:px-32 pt-20">
+    <div className="px-3 lg:px-32 pt-20 pb-10">
       <h1 className="text-4xl text-center">Klantverhalen</h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 pt-10 ">
+        <StoriesCard></StoriesCard>
+        <StoriesCard></StoriesCard>
+        <StoriesCard></StoriesCard>
+      </div>
     </div>
   );
 }
--- src/components/Home/Stories/StoriesCard.tsx
+++ src/components/Home/Stories/StoriesCard.tsx
@@ -1,3 +1,22 @@
 export default function StoriesCard() {
-  return <div>StoriesCard</div>;
+  return (
+    <div
+      className=""
+      style={{
+        backgroundImage: `url('/home/stories/image-1.png')`,
+        height: "388px",
+        color: "white",
+      }}
+    >
+      <div className="card-body mt-[50%] ">
+        <div className="border-l-4 border-[#77892B] pl-3">
+          <h2 className="card-title ">Boerderij De Groene Hoop</h2>
+          <p>
+            De gemeente Natuurstad wilde haar stedelijke omgeving vergroenen en
+            biodiversiteit bevorderen. Met onze hulp hebben ze parken en.....
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
